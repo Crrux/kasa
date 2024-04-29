fd94498a39061376f55779f433f868c59655edd6
Mursch Arthur <arthur.mursch@gmail.com>
2024-04-29 20:25:17 +0200

    maj card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,38 @@
-import './card.css'
+import { useState, useEffect } from 'react'
 
 function Card() {
-  async function fetchjson() {
-    const response = await fetch('../src/database.json')
-    const logements = await response.json()
-    console.log(logements[0])
-    return (
-      <article>
-        <img src={logements[0].cover}></img>
-        <h2>{logements[0].title}</h2>
-      </article>
-    )
-  }
-  fetchjson()
+  const [logements, setLogements] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('../src/database.json')
+        const data = await response.json()
+        setLogements(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      {logements.length > 0 ? (
+        logements.map((logement) => (
+          <article key={logement.id}>
+            {' '}
+            {/* Add a unique key for each item */}
+            <img src={logement.cover} alt={logement.title} />
+            <h2>{logement.title}</h2>
+          </article>
+        ))
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  )
 }
 
 export default Card
