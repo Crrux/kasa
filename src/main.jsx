87de3a0af2afbd0820ed1c46f31fe8b06827d6d9
Mursch Arthur <arthur.mursch@gmail.com>
2024-04-29 13:43:54 +0200

    Routing + config prettier + suppression fichier inutile

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/home'
+import Fiche from './pages/fiche_logement/fichelogement.jsx'
+import Notfound from './pages/NotFound/NotFound.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fichelogement" element={<Fiche />} />
+        <Route path="/404NotFound" element={<Notfound />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
---
+++ src/pages/APropos/APropos.jsx
@@ -0,0 +1,9 @@
+function apropos() {
+  return (
+    <div>
+      <h1>a propos</h1>
+    </div>
+  )
+}
+
+export default apropos
