84ccc0ec1326d4a8cde53370e46e1486cb128272
Mursch Arthur <arthur.mursch@gmail.com>
2024-04-30 17:07:06 +0200

    recuperation données logement cliqué

--- src/pages/fiche_logement/fichelogement.jsx
+++ src/pages/fiche_logement/fichelogement.jsx
@@ -1,11 +1,39 @@
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+function getLogement(idLogement, logements) {
+  let ficheLogement = []
+  for (let i = 0; i < logements.length; i++) {
+    if (logements[i].id === idLogement) {
+      ficheLogement = logements[i]
+    }
+  }
+  return ficheLogement
+}
 
 function Fiche() {
   let { id } = useParams()
+  const [logements, setLogements] = useState([])
+  useEffect(() => {
+    const dataLogement = async () => {
+      try {
+        const response = await fetch('../src/database.json')
+        const data = await response.json()
+        setLogements(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    dataLogement()
+  }, [])
+  let ficheLogement = getLogement(id, logements)
+  console.log(id)
+  console.log(ficheLogement)
+
   return (
     <main>
-      <h1>fiche logement</h1>
-      <p>{id}</p>
+      <h1>{ficheLogement.title}</h1>
+      <p></p>
     </main>
   )
 }
