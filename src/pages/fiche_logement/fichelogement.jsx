dbb022b21519a7b2095bf95110468b81b30fb64f
Mursch Arthur <arthur.mursch@gmail.com>
2024-05-02 14:15:06 +0200

    changement fonction tri + condition d'affichage

--- src/pages/fiche_logement/fichelogement.jsx
+++ src/pages/fiche_logement/fichelogement.jsx
@@ -1,56 +1,45 @@
-import { useParams } from 'react-router-dom'
+import { useParams, redirect } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-
-function getLogement(idLogement, logements) {
-  let ficheLogement = []
-  for (let i = 0; i < logements.length; i++) {
-    if (logements[i].id === idLogement) {
-      ficheLogement = logements[i]
-    }
-  }
-  return ficheLogement
-}
 
 function Fiche() {
   let { id } = useParams()
-  const [logements, setLogements] = useState([])
+  const [logement, setLogements] = useState([])
 
   useEffect(() => {
     const dataLogement = async () => {
       try {
         const response = await fetch('/database/database.json')
         const data = await response.json()
-        /* data.filter() */
-        setLogements(data)
+        const logement = data.find((logement) => logement.id === id)
+        setLogements(logement)
+        if (!logement) {
+          return redirect('/404')
+        }
       } catch (error) {
         console.error('Error fetching data:', error)
         /*  Ici gérer la redirection vers la page erreur */
       }
     }
     dataLogement()
-  }, [])
+  }, [logement, id])
 
-  let ficheLogement = getLogement(id, logements)
-  let hostname = ''
-  let srcPicture = ''
-  if (ficheLogement.host) {
-    hostname = ficheLogement.host.name
-    srcPicture = ficheLogement.host.picture
-  } else {
-    hostname = 'Chargement...'
-    srcPicture = ''
-  }
   return (
     <main>
-      <img src={ficheLogement.cover}></img>
-      <div>
-        <h1>{ficheLogement.title}</h1>
-        <h2>{ficheLogement.location}</h2>
-      </div>
-      <div>
-        <p>{hostname}</p>
-        <img src={srcPicture} />
-      </div>
+      {logement ? (
+        <>
+          <img src={logement.cover}></img>
+          <div>
+            <h1>{logement.title}</h1>
+            <h2>{logement.location}</h2>
+          </div>
+          <div>
+            <p>{logement.host?.name || 'Chargement...'}</p>
+            <img src={logement.host?.picture || ''} />
+          </div>
+        </>
+      ) : (
+        <div>Chargement en cours...</div>
+      )}
     </main>
   )
 }
