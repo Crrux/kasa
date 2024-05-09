6976387da7e6b0ad3eaa8be6de8ffdd0413a092b
Mursch Arthur <arthur.mursch@gmail.com>
2024-05-09 13:31:54 +0200

    indexation rajouter

--- src/components/caroussel/Caroussel.jsx
+++ src/components/caroussel/Caroussel.jsx
@@ -14,7 +14,7 @@ function Carousel({ data }) {
 
   const showSlides = (n) => {
     let i
-    const slides = document.getElementsByClassName('mySlides')
+    const slides = document.getElementsByClassName('myPictures')
 
     if (n > slides.length) {
       setSlideIndex(1)
@@ -27,31 +27,44 @@ function Carousel({ data }) {
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none'
     }
-    slides[n - 1].style.display = 'flex'
+    slides[n - 1].style.display = 'block'
   }
-
   return (
-    <section>
-      <div className="slideshow-container">
+    <section className="slideshow-container">
+      <div className="mySlides">
         {data ? (
           <>
             {data.map((pictures) => (
-              <div key={pictures} className="mySlides fade">
-                <img src={pictures} />
+              <div key={pictures} className="myPictures fade">
+                <div className="overflowtest">
+                  <img src={pictures} />
+                </div>
+
+                <p className="myPictures--indexation">
+                  {slideIndex}/{data.length}
+                </p>
               </div>
             ))}
           </>
         ) : (
           ''
         )}
+      </div>
 
+      {data.length > 1 ? (
         <button className="prev" onClick={() => plusSlides(-1)} type="button">
           &#10094;
         </button>
+      ) : (
+        ''
+      )}
+      {data.length > 1 ? (
         <button className="next" onClick={() => plusSlides(1)} type="button">
           &#10095;
         </button>
-      </div>
+      ) : (
+        ''
+      )}
     </section>
   )
 }
